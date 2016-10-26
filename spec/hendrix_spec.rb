4dab08d79ee308267a5458ab4bfdbe486844ca53
Marcelo Boeira <me@marceloboeira.com>
2016-10-26 10:06:51 +0200

    test: Add context to scenarios
    Improve readability

--- spec/hendrix_spec.rb
+++ spec/hendrix_spec.rb
@@ -4,18 +4,22 @@ RSpec.describe "Hexdrix" do
   let(:peer_a) { TCPSocket.new("localhost", 9827) }
   let(:peer_b) { TCPSocket.new("localhost", 9827) }
 
-  it "echoes a message" do
-    peer_a.puts("foo")
-    message = peer_a.gets
+  context "when only a single peer is connected" do
+    it "echoes a message" do
+      peer_a.puts("foo")
+      message = peer_a.gets
 
-    expect(message).to eq("foo\n")
+      expect(message).to eq("foo\n")
+    end
   end
 
-  it "echoes a message to the correct connection only" do
-    peer_a.puts("foo")
-    peer_b.puts("bar")
-    message = peer_b.gets
+  context "when multiple peers are conected" do
+    it "echoes a message to the correct peer only" do
+      peer_a.puts("foo")
+      peer_b.puts("bar")
+      message = peer_b.gets
 
-    expect(message).to eq("bar\n")
+      expect(message).to eq("bar\n")
+    end
   end
 end
