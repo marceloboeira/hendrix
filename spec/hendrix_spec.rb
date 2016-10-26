e06c262e0b3ec29004757b91e8eee3ddf397186c
Marcelo Boeira <me@marceloboeira.com>
2016-10-26 10:00:10 +0200

    test: Ensure the echo to the correct peer only
    Only the peer that send the message should get the echo

--- spec/hendrix_spec.rb
+++ spec/hendrix_spec.rb
@@ -1,12 +1,21 @@
 require "socket"
 
 RSpec.describe "Hexdrix" do
-  let(:server) { TCPSocket.new("localhost", 9827) }
+  let(:peer_a) { TCPSocket.new("localhost", 9827) }
+  let(:peer_b) { TCPSocket.new("localhost", 9827) }
 
   it "echoes a message" do
-    server.puts("foo")
-    message = server.gets
+    peer_a.puts("foo")
+    message = peer_a.gets
 
     expect(message).to eq("foo\n")
+  end
+
+  it "echoes a message to the correct connection only" do
+    peer_a.puts("foo")
+    peer_b.puts("bar")
+    message = peer_b.gets
+
+    expect(message).to eq("bar\n")
   end
 end
